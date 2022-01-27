d65895a6efdc6a6d48c9db60d83227cc6d1460b8
parth <parth@mail.com>
2022-01-27 14:03:56 +0530

    basics of implementations

--- week1/Implementation/lunch_box/script.js
+++ week1/Implementation/lunch_box/script.js
@@ -2,10 +2,12 @@ function maximumSchools() {
     let boxes = Number(document.getElementById('boxes').value);
     const school = document.getElementById('school').value;
     const schoolArray = school.split(' ')
+
+    //sort the array 
     schoolArray.sort((a,b) => a-b)  
     let result = 0;
     for(let i =0 ;i<schoolArray.length;i++){
-        if (boxes >= schoolArray[i]) {
+        if (boxes >= schoolArray[i]) { // reduse the boxes so get idea to maximum number of school
             result ++;
             boxes -= schoolArray[i];
         }
